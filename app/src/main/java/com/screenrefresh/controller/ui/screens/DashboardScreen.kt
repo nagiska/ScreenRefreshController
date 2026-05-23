16b7378ee8a42055f9690f0d6fe9d9cc7d83b292
root <root@iZ7xvccvb38j7bvyx5kxjjZ>
2026-05-23 20:17:02 +0800

    fix: RateButton extends RowScope for weight modifier

--- app/src/main/java/com/screenrefresh/controller/ui/screens/DashboardScreen.kt
+++ app/src/main/java/com/screenrefresh/controller/ui/screens/DashboardScreen.kt
@@ -6,6 +6,7 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -224,7 +225,7 @@ fun DashboardScreen(
 }
 
 @Composable
-fun RateButton(
+fun RowScope.RateButton(
     rate: Int,
     curRate: Int,
     scope: kotlinx.coroutines.CoroutineScope,
